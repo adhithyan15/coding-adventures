027f30ae0be50fbd08da394b6097f5ea8afcc6b4
Adhithya Rajasekaran <adhithyan15@users.noreply.github.com>
2026-03-23 22:13:34 -0700

    refactor(typescript): wrap shared-source lexer

--- code/packages/typescript/lexer/vitest.config.ts
+++ code/packages/typescript/lexer/vitest.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
+      include: [
+        "src/**/*.ts",
+      ],
+      exclude: [
+        "src/index.ts",
+        "src/token.ts",
+      ],
       provider: "v8",
       thresholds: {
         lines: 80,
