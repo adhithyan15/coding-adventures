59906a096bba4c03f0420526d5cf82334e11b0e7
Adhithya Rajasekaran <adhithyan15@gmail.com>
2026-04-25 00:40:00 -0700

    feat(parity): add radix tree packages

---
+++ code/packages/ruby/radix_tree/test/test_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "simplecov"
+SimpleCov.start do
+  enable_coverage :branch
+  minimum_coverage 95
+end
+
+require "minitest/autorun"
+require_relative "../lib/coding_adventures_radix_tree"
---
+++ code/packages/typescript/radix-tree/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## 0.1.0
+
+- Initial TypeScript radix tree package with compressed edges, insertion,
+  deletion with merge/prune, prefix lookup, longest-prefix matching, and sorted
+  key/value enumeration.
