18b53eb4d974a8988b2fc134eeb548285ef7b816
Adhithya Rajasekaran <adhithyan15@users.noreply.github.com>
2026-04-25 12:00:58 -0700

    feat(parity): add python in-memory data store protocol (#1352)

---
+++ code/packages/python/in-memory-data-store-protocol/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 0.1.0
+
+- Initial Python parity package for in-memory data store protocol frames.
---
+++ code/packages/python/in-memory-data-store-protocol/src/in_memory_data_store_protocol/__init__.py
@@ -0,0 +1,5 @@
+"""Protocol IR for the in-memory data store."""
+
+from in_memory_data_store_protocol.protocol import CommandFrame, EngineResponse, ascii_upper
+
+__all__ = ["CommandFrame", "EngineResponse", "ascii_upper"]
