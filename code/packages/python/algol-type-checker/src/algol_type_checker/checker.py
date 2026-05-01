b7c3a9bd41ea82fbe5b1e9f8284d3cbddfc71db1
Adhithya Rajasekaran <adhithyan15@users.noreply.github.com>
2026-04-30 20:41:44 -0700

    Forward array-element actuals through formal procedures (#1851)

--- code/packages/python/algol-type-checker/src/algol_type_checker/checker.py
+++ code/packages/python/algol-type-checker/src/algol_type_checker/checker.py
@@ -2174,6 +2174,8 @@ class AlgolTypeChecker:
                 and self._resolved_bare_procedure_expression(argument) is None
             )
             procedure_argument_ids.append(None)
+            if argument_assignable[-1]:
+                self._record_assignable_actual_write(argument, scope)
             if actual_type != ERROR and actual_type not in {
                 INTEGER,
                 BOOLEAN,
